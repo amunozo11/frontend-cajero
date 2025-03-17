dd957b06d6a094bbe2303034f91d22896fb8105c
Tu Nombre <tu_email@example.com>
2025-03-17 14:42:32 -0500

    Corrigiendo configuración de Vite y Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -11,7 +12,10 @@ export default defineConfig({
   },
   css: {
     postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')],
+      plugins: [
+        require('tailwindcss'), // Asegúrate de que TailwindCSS esté aquí
+        require('autoprefixer'), // Y Autoprefixer también
+      ],
     },
   },
 });
